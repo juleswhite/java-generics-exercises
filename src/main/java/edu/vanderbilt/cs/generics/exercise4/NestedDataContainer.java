a913a1867de3aebdc73f79e07fa67b07934cf23f
Jules White <jules.white@gmail.com>
2020-11-22 06:28:35 -0600

    Clarified instructions in NestedDataContainer

--- src/main/java/edu/vanderbilt/cs/generics/exercise4/NestedDataContainer.java
+++ src/main/java/edu/vanderbilt/cs/generics/exercise4/NestedDataContainer.java
@@ -12,6 +12,10 @@ import java.util.List;
  * Refactor the NestedDataContainer to be parameterized in a way
  * that the NestedDataContainerTest compiles and passes.
  *
+ * You should look at the NestedDataContainerTest to understand
+ * how many type parameters are expected and what the return
+ * type of getData should be.
+ *
  * This one will really tests your understanding of generics!
  *
  * Once you complete this exercise, you should have a solid
@@ -25,9 +29,13 @@ public class NestedDataContainer {
     private DataContainer data = new DataContainer();
 
     public void setNestedData(Object nestedData){
-        AttributedDataContainer c = new AttributedDataContainer();
-        c.setData(nestedData);
-        data.setData(c);
+        // @ToDo
+        //
+        // Uncomment and fix the code below.
+        //
+//        AttributedDataContainer c = new AttributedDataContainer();
+//        c.setData(nestedData);
+//        data.setData(c);
     }
 
     public AttributedDataContainer getData() {
